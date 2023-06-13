341d5c38614c8ccff68bdf42272c316044477484
ilitzkyd <ilitzkyd@berkeley.edu>
2023-06-13 02:35:40 -0700

    Added error suggestions

--- scrabble.py
+++ scrabble.py
@@ -17,18 +17,30 @@ def find_matching_words(rack, data):
                 break 
         if valid_word: 
             matching_words.append(word) #A valid word does get added to the list of matching words 
-    
+        if len(rack) == 2 and all(char in ("*", "?") for char in rack):
+            return [] 
     return matching_words
 
 
-def run_scrabble(word):    
-    with open("C:\\Users\\davidilitzky.REDMOND\\Berkeley\\sowpods.txt", "r") as infile: #Reads the sowpods text file in local directory 
-        raw_input = infile.readlines()
-        data = [datum.strip('\n') for datum in raw_input]
-
+def run_scrabble(word): 
+    file_path="C:\\Users\\davidilitzky.REDMOND\\Berkeley\\sowpods.txt"
+    try:
+        with open(file_path, "r") as infile: #Reads the sowpods text file in local directory 
+            raw_input = infile.readlines()
+            data = [datum.strip('\n') for datum in raw_input]
+    except FileNotFoundError:
+        print(f"Error: The sowpods.txt file at '{file_path}' could not be found.")
+        print("Please input the correct path")
+        return[],0
+    if not all(char.isalpha() or char in ("*", "?") for char in word):
+        print("Error: The word should contain alphabetical characters or wildcards (*,?)")
+        print("Please enter the word again by removing the non-alphabetical letters")
+        return[],0
 
     matching_words = find_matching_words(word, data) #call the matching words function to text file and stores them 
-
+    if not matching_words:
+        return [], 0
+    
     word_scores = [(word, score_word(word)) for word in matching_words]
 
     # Sort the list by score in descending order
@@ -41,6 +53,7 @@ def run_scrabble(word):
             grouped_words.append((score, word))
     print(((grouped_words),len(matching_words))) #Prints the matching words and the count of how many exist
 
+    return grouped_words,len(matching_words)
 
 
 def main (): 
