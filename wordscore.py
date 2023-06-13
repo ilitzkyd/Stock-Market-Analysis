c7c8f23d2a09128cd061a08307d3fa33d6e21154
ilitzkyd <ilitzkyd@berkeley.edu>
2023-06-13 01:44:43 -0700

    Wordscore python file

---
+++ wordscore.py
@@ -0,0 +1,20 @@
+scores = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2,
+         "f": 4, "i": 1, "h": 4, "k": 5, "j": 8, "m": 3,
+         "l": 1, "o": 1, "n": 1, "q": 10, "p": 3, "s": 1,
+         "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4,
+         "x": 8, "z": 10}
+
+def score_word(word):
+     if word == "**" or word == "??":
+        return 0
+     total_score = 0
+     for letter in word:
+        if letter == "*" or letter == "?":
+            total_score += 0
+        else:
+            total_score += scores.get(letter.lower(), 0)
+        return total_score
+  
+
+
+
