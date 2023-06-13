30055cbdbb04bed968f85f133a69dc7140222727
ilitzkyd <ilitzkyd@berkeley.edu>
2023-06-13 09:18:27 -0700

    Changed functions

--- scrabble.py
+++ scrabble.py
@@ -23,52 +23,15 @@ def find_matching_words(rack, data):
 
 
 def run_scrabble(word=None):
-    if word is None:
-        return "Error: No input has been provided. Please enter a rack", ""
-
-    rack = word.upper()
-
-    if not all(char.isalpha() or char in ("*", "?") or char.isdigit() for char in word):
-        return "Error: The word should contain alphabetical characters or wildcards (*, ?). Please enter the word again by removing the non-alphabetical letters", ""
-
-    if not all(char.isalpha() or char in ("*", "?") or char.isdigit() for char in rack):
-        return "Error: The letter rack should contain alphabetical characters or wildcards (*, ?). Please enter the rack again by removing the non-alphabetical letters", ""
-
-    if len(rack) == 1:
-        return "Error: The rack should be more than a letter. Please input more than 1 letter", ""
-
-    if rack.count('*') + rack.count('?') > 2:
-        return "Error: Rack cannot have more than 2 wildcards. Please only have 2 wildcards", ""
-
-    if len(rack) > 7:
-        return "Error: Rack cannot have more than 7 letters. Please only have 7 letters", ""
-
-    if word == "*?":
-        return 0, 0
-
-    with open("sowpods.txt", "r") as infile:
-        raw_input = infile.readlines()
-        data = [datum.strip('\n') for datum in raw_input]
-
-    matching_words = find_matching_words(word, data)
-    if not matching_words:
-        return "", 0
-
-    word_scores = [(word, score_word(word)) for word in matching_words]
-    word_scores.sort(key=lambda x: x[1], reverse=True)
-
-    if len(word_scores) == 1 and word_scores[0][1] == 0:
-        return 0, len(matching_words)
-
-    grouped_words = []
-    for word, score in word_scores:
-        count = word_scores.count((word, score))
-        if score == 0:
-            continue  # Skip adding to grouped_words if the score is 0
-        if (score, word) not in grouped_words:
-            grouped_words.append((score, word))
-
-    return grouped_words, len(matching_words)
+    """
+    Runs the Scrabble game for a given word or letter rack.
+    The function checks the validity of the input word or rack, performs error checks,
+    and returns error messages if necessary. If the input is valid, it proceeds to find
+    matching words and calculates their scores.
+    Returns:
+            - A list of grouped words and their scores, sorted in descending order.
+            - The count of matching words.
+    """
 
     if word is None:
         return "Error: No input has been provided. Please enter a rack", ""
@@ -92,7 +55,7 @@ def run_scrabble(word=None):
 
     if word == "*?":
         return 0, 0
-
+#"C:\\Users\davidilitzky.REDMOND\\Berkeley\\sowpods.txt
     with open("sowpods.txt", "r") as infile:
         raw_input = infile.readlines()
         data = [datum.strip('\n') for datum in raw_input]
@@ -118,15 +81,9 @@ def run_scrabble(word=None):
     return grouped_words, len(matching_words)
 
 
-
-def main():
-    word = "aaaaaaaaaa"
-    if word == "*?":
-        print(0)
-    else:
-        result = run_scrabble(word)
-        print(result)
-
-
-if __name__ == "__main__":
-    main()
+word = "ZAEfiee"
+if word == "*?":
+    print(0)
+else:
+    result = run_scrabble(word)
+    print(result)
