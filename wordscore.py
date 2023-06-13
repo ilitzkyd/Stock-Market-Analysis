975420767466e79a216556b16e9bbd0cb67d8b7d
ilitzkyd <ilitzkyd@berkeley.edu>
2023-06-13 04:10:20 -0700

    Added docstring

--- wordscore.py
+++ wordscore.py
@@ -4,11 +4,21 @@ scores = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2,
          "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4,
          "x": 8, "z": 10}
 
+
 def score_word(word):
-    total_score = 0
-    for letter in word:
-        total_score += scores.get(letter.lower(), 0)
-    return total_score
+     """
+    Calculates the score of a given word based on letter scores.
+
+    The function calculates the score of a word by summing the individual scores
+    of its letters. The scores for each letter are defined in the 'scores' dictionary.
+
+    Args:
+        word (str): The word for which to calculate the score.
+    """
+     total_score = 0
+     for letter in word:
+         total_score += scores.get(letter.lower(), 0)
+     return total_score
 
 
 
