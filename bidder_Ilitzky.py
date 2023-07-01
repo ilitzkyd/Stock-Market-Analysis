346389e43d91eb3ac48c9c0e596a43ba3656d4e9
ilitzkyd <ilitzkyd@berkeley.edu>
2023-07-01 07:23:01 -0700

    Changed function

--- bidder_Ilitzky.py
+++ bidder_Ilitzky.py
@@ -30,8 +30,5 @@ class Bidder:
         if auction_winner and clicked is not None:
             if self.balances:
                 user_id = next(iter(self.balances))
-            if user_id in self.balances:
                 if len(self.balances[user_id]) > 0:
-                    self.balances[user_id].append(self.balances[user_id][-1] - price)
-                else:
-                    self.balances[user_id].append(-price)
+                    self.balances[user_id][-1] -= price
