ae3aa611912bb03278de6d0ec5642169f70cfff9
ilitzkyd <ilitzkyd@berkeley.edu>
2023-07-01 05:28:52 -0700

    Added docstrings

--- bidder_lastname.py
+++ bidder_lastname.py
@@ -1,17 +1,29 @@
 import random
 class Bidder:
+    """
+    Represents the bidder participating in the auction 
+    """
     def __init__(self, num_users, num_rounds):
+        """
+        Initializes the bidder objects with user participation and rounds
+        """
         self.num_users = num_users
         self.num_rounds = num_rounds
         self.balances = {}
 
     def bid(self, user_id):
+        """
+        Places a bid for user
+        """
         max_bid = 1000
         min_bid = 0
         total_bid = round(random.uniform(min_bid, max_bid), 3)
         return total_bid
 
     def notify(self, auction_winner, price, clicked):
+        """
+        Lets the user know of their outcome
+        """
         if auction_winner and clicked is not None:
             if self.balances:
                 user_id = next(iter(self.balances))
